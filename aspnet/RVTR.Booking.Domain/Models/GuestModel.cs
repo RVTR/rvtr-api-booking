5bbf061338283ff326a47dbe9f92e01dca5b362a
chegior <chegiorca@hotmail.com>
2021-02-11 14:14:04 -0700

    Add Properties to Guest Model

--- aspnet/RVTR.Booking.Domain/Models/GuestModel.cs
+++ aspnet/RVTR.Booking.Domain/Models/GuestModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -6,6 +7,9 @@ namespace RVTR.Booking.Domain.Models
   public class GuestModel : AEntity, IValidatableObject
   {
     public int? BookingModelId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsMinor { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => new List<ValidationResult>();
   }
