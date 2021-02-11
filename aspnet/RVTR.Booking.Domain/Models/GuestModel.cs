95aeeddeb61baed81576c9f60983fb86908e7e53
chegior <chegiorca@hotmail.com>
2021-02-11 15:52:04 -0700

    Added properties to the Guest Model as requested

--- aspnet/RVTR.Booking.Domain/Models/GuestModel.cs
+++ aspnet/RVTR.Booking.Domain/Models/GuestModel.cs
@@ -7,6 +7,9 @@ namespace RVTR.Booking.Domain.Models
   {
     public int Id { get; set; }
     public int? BookingModelId { get; set; }
+    public string FirstName { get; set; }
+    public string LasstName { get; set; }
+    public bool IsMinor { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => new List<ValidationResult>();
   }
