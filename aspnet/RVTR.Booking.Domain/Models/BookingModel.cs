470bd17c409114b1a5cf6ae2deb88ac289f1eb4a
chegior <chegiorca@hotmail.com>
2021-02-13 11:19:46 -0700

    Issue#51,  Unique number 20 digit type long for Booking Number using Tick and Ramdon       //Random was added to avoid duplicate value when two users dothe booking

--- aspnet/RVTR.Booking.Domain/Models/BookingModel.cs
+++ aspnet/RVTR.Booking.Domain/Models/BookingModel.cs
@@ -23,6 +23,7 @@ namespace RVTR.Booking.Domain.Models
     public DateTime CheckIn { get; set; }
 
     public DateTime CheckOut { get; set; }
+    public string BookingNumber { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
