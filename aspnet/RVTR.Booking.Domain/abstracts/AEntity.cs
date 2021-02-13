d9777f57e21c6187be5dfde98d8a9bb1696d84d3
Nar <rohini.smi2010@gmail.com>
2021-02-13 12:32:14 -0600

    Issue# 61. Add xml comments for EntityId

--- aspnet/RVTR.Booking.Domain/abstracts/AEntity.cs
+++ aspnet/RVTR.Booking.Domain/abstracts/AEntity.cs
@@ -3,7 +3,7 @@ namespace RVTR.Booking.Domain
    public class AEntity
    {
       /// <summary>
-      /// Leverage abstraction to the Id for models
+      /// Leverage abstraction to the Id for models. All models need to be inherited from AEntity
       /// </summary>
       public int EntityId { get; set; }
       public AEntity()
