ec241e6edc55a300167be442c312dee2733c9814
Nar <rohini.smi2010@gmail.com>
2021-02-13 11:55:05 -0600

    XML comments added for the AEntity class. #61

--- aspnet/RVTR.Booking.Domain/abstracts/AEntity.cs
+++ aspnet/RVTR.Booking.Domain/abstracts/AEntity.cs
@@ -2,7 +2,9 @@ namespace RVTR.Booking.Domain
 {
    public class AEntity
    {
-
+      /// <summary>
+      /// Leverage abstraction to the Id for models
+      /// </summary>
       public int EntityId { get; set; }
       public AEntity()
       {
